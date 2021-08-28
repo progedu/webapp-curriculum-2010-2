bfe6e80fbb86b190215319a6d8ea978a8fbd9b92
sabasaikyouno <0852kuro@gmail.com>
2021-08-28 18:41:16 +0900

    Update ReflectionChallenge.scala

--- src/main/scala/ReflectionChallenge.scala
+++ src/main/scala/ReflectionChallenge.scala
@@ -7,6 +7,12 @@ class Issue(private val title: String) {
 object ReflectionChallenge extends App {
     val issue = new Issue("不具合1")
 
-      // TODO リフレクションを用いて printTitle() メソッドを呼び出す
+    val m = runtimeMirror(issue.getClass.getClassLoader)
+
+    val im = m.reflect(issue)
+
+    val issueMirror = im.reflectMethod(typeOf[Issue].decl(TermName("printTitle")).asMethod)
+
+    issueMirror.apply(0)
 }
 
